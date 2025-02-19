e97dc1bd7569d211c33780b4941126722691b812
sreerag <sreeragtv007@gmail.com>
2025-02-19 11:27:04 +0530

    discover_event_added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Events(models.Model):
+    
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    discription = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=50)
+    event_img = models.ImageField(upload_to='event_image')
+    price = models.IntegerField()
+    
+    
+    class Meta:
+        ordering = ['date']
+    
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -9,3 +9,9 @@ from rest_framework.response import Response
 class home_page(APIView):
     def get(self,request):
         return Response({'msg':'home page'})
+    
+class events(APIView):
+    
+    def get(self,request):
+        
+        return Response('all events')
